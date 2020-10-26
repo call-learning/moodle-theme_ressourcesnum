cc9df19f75937f7d681b89cdb6cae7928625d143
Laurent David <lmedavid@gmail.com>
2020-10-26 18:46:57 +0100

    Add French language translation for theme

---
+++ lang/fr/theme_ressourcesnum.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file
+ *
+ * @package   theme_ressourcesnum
+ * @copyright 2020 - CALL Learning - Laurent David <laurent@call-learning>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addresses'] = 'Addresse pour le pied de page';
+$string['addresses_desc'] = 'Addresses pour le pied de page.  <br/>Structure:
+     <ul>
+     <li>addresslabel|address|tel</li>
+     <li>Exemple: HESAM Université|15, rue Soufflot<br>75005 Pars|01 87 39 20 20</li>
+     </ul>';
+$string['address'] = 'Addresse';
+
+$string['additionalinfo'] = 'Information additionnelle';
+
+$string['nobootswatch'] = 'None';
+$string['pluginname'] = 'Ressource Numériques';
+$string['configtitle'] = 'Theme Ressource Numériques';
+$string['logos'] = 'Logos';
+$string['logos_desc'] = 'Logos des organisation associées, affichées dans le pied de page.';
+$string['mesri'] = 'Ministère de l\'Enseignement supérieur, de la Recherche et de l\'Innovation';
+
+$string['republiquefrancaise'] = 'République Française';
+$string['cookiesrgpd'] = 'Gestion des cookies et des données';
+$string['mentionlegales'] = 'Mentions légales';
+$string['copyright'] = '© HESAM  2020';
+
+$string['region-side-pre'] = 'Droite';
+$string['region-content'] = 'Content';
+
+$string['sitemap'] = 'Plan du site';
+$string['tel'] = 'Téléphone';
